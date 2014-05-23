d23cd8385a7070c6154166cd634323c0e49799fa
Reginaldo Castardo <rcastardo@gmail.com>
2014-05-23 16:53:16 -0300

    Class request post get

---
+++ src/Classes/Request.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rcastardo
+ * Date: 23/05/14
+ * Time: 16:51
+ */
+
+namespace ReginaldoTeste\Classes;
+
+
+class Request {
+
+    public function __construct()
+    {
+        
+    }
+
+} 
